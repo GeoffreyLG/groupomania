f5ee7e56ba40d99cac37d3a3a7bcff0515b142a9
GeoffreyLG <geoffrey.legac@gmail.com>
2021-01-31 18:02:30 +0100

    New vues

---
+++ groupomania/src/components/connexion.vue
@@ -0,0 +1,14 @@
+<v-container>
+  <v-row class="text-center">
+    <v-col>
+      <v-btn elevation="2" color="secondary">
+        S'inscrire
+      </v-btn>
+    </v-col>
+    <v-col>
+      <v-btn elevation="2" color="secondary">
+        Se connecter
+      </v-btn>
+    </v-col>
+  </v-row>
+</v-container>
