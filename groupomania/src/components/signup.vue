9604db75ddf4e1a1520d5134a7a39c3df8075d39
GeoffreyLG <geoffrey.legac@gmail.com>
2021-02-01 16:05:39 +0100

    Signup with buttons

--- groupomania/src/components/signup.vue
+++ groupomania/src/components/signup.vue
@@ -15,18 +15,45 @@
                                             v-model="email"
                                             type="email"
                                             required
-                                        ></v-text-field> 
+                                        ></v-text-field>
+                                    </v-flex>
+                                </v-layout>
+                                <v-layout row>
+                                    <v-flex xs12>
                                         <v-text-field
                                             name="password"
                                             label="Password"
-                                            required
-                                        ></v-text-field>
-                                        <v-text-field
-                                            name="confirmPassword"
-                                            label="Confirm password"
+                                            id="password"
+                                            v-model="password"
+                                            type="password"
                                             required
                                         ></v-text-field>
                                     </v-flex>
+                                </v-layout>
+                                <v-layout row>
+                                    <v-flex xs12>
+                                        <v-text-field
+                                            name="confirmPassword"
+                                            label="Confirm password"
+                                            id="confirmPassword"
+                                            v-model="password"
+                                            type="password"
+                                        ></v-text-field>
+                                    </v-flex>
+                                </v-layout>
+                                <v-layout>
+                                    <v-btn elevation="2" rounded to="/" class="mr-4 mt-5">
+                                        Cancel
+                                    </v-btn>
+                                    <v-btn
+                                        elevation="2"
+                                        rounded 
+                                        type="submit"
+                                        color="info" 
+                                        class="mt-5"
+                                    >
+                                        Créer mon compte
+                                    </v-btn>
                                 </v-layout>
                             </v-form>
                         </v-container>
