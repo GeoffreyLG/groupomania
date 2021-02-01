0643a9a0a23be6743b8257e382fd5c3893629400
GeoffreyLG <geoffrey.legac@gmail.com>
2021-02-01 15:38:30 +0100

    Add initialize firebase

--- groupomania/src/main.js
+++ groupomania/src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import vuetify from './plugins/vuetify';
+import * as firebase from 'firebase'
 
 Vue.use(vuetify)
 Vue.config.productionTip = false
@@ -9,5 +10,14 @@ Vue.config.productionTip = false
 new Vue({
   router,
   vuetify,
-  render: h => h(App)
+  render: h => h(App),
+  created () {
+    firebase.initializeApp({
+      cleApiWeb: 'AIzaSyAXOWArN4sCrH7l4Am4EYFBAoJZw1YNN9M',
+      authDomain:	'groupomania-34-default-rtdb.firebaseio.com',
+      databaseURL:	'https://groupomania-34-default-rtdb.firebaseio.com',
+      projectId:	'groupomania-34',
+      storageBucket: '',
+    })
+  }
 }).$mount('#app')
