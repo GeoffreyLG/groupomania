66bf9391d1938d777edceb3ae42379c2aa41ed19
GeoffreyLG <geoffrey.legac@gmail.com>
2021-02-01 01:29:02 +0100

    All OK !

--- groupomania/src/App.vue
+++ groupomania/src/App.vue
@@ -1,41 +1,24 @@
 <template>
   <v-app>
-      <v-container>
-        <v-row class="text-center">
-          <v-col cols="12">
-            <v-img
-              :src="require('@/assets/logo.svg')"
-              class="my-3"
-              contain
-              height="200"
-            />
-          </v-col>
-
-          <v-col class="mb-4">
-            <h1 class="display-2 font-weight-bold mb-3">
-              Welcome to Groupomania
-            </h1>
-          </v-col>
-        </v-row>
-      </v-container>
-      <v-container>
-        <v-row class="text-center">
-          <v-col>
-            <v-btn elevation="2" rounded color="secondary">
-              Se connecter
-            </v-btn>
-          </v-col>
-        </v-row>
-        <v-row class="text-center">
-          <v-col>
-            <v-btn elevation="2" rounded color="secondary">
-              S'inscrire
-            </v-btn>
-          </v-col>
-        </v-row>
-      </v-container>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+    <v-btn elevation="2" rounded to="/signin">Connexion</v-btn>
+    </v-app-bar>
     <v-main>
-      
+      <router-view></router-view>
     </v-main>
   </v-app>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
