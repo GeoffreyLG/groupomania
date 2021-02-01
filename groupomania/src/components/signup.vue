39a990b1988e33ac251b03e676516cf04240d075
GeoffreyLG <geoffrey.legac@gmail.com>
2021-02-01 15:55:07 +0100

    New signup process

---
+++ groupomania/src/components/signup.vue
@@ -0,0 +1,38 @@
+<template>
+    <v-container>
+        <v-layout>
+            <v-flex xs12 sm6 offset-sm3>
+                <v-card>
+                    <v-card-text>
+                        <v-container>
+                            <v-form>
+                                <v-layout row>
+                                    <v-flex xs12>
+                                        <v-text-field
+                                            name="email"
+                                            label="E-mail"
+                                            id="email"
+                                            v-model="email"
+                                            type="email"
+                                            required
+                                        ></v-text-field> 
+                                        <v-text-field
+                                            name="password"
+                                            label="Password"
+                                            required
+                                        ></v-text-field>
+                                        <v-text-field
+                                            name="confirmPassword"
+                                            label="Confirm password"
+                                            required
+                                        ></v-text-field>
+                                    </v-flex>
+                                </v-layout>
+                            </v-form>
+                        </v-container>
+                    </v-card-text>
+                </v-card>
+            </v-flex>
+        </v-layout>
+    </v-container>
+</template>
--- groupomania/src/router/index.js
+++ groupomania/src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/views/Home'
 import Signin from '@/views/signin'
-import Subscribe from '@/views/subscribe'
+import Subscribe from '@/components/signup'
 Vue.use(Router)
 
 export default new Router({
